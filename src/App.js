23f299d840e8e468880180e16922d40bb28d5488
Francis Maina <sebastianfrancix@gmail.com>
2022-09-29 12:44:28 +0300

    renders our data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
-        <h3>0 Birthdays Today</h3>
-        <List />
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
         <button type="button" onClick={() => console.log("I was clicked")}>
           Clear All
         </button>
