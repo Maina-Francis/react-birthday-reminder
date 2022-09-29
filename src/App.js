efbc80674c3c91d508d74bbc44b452c7aeb3000a
Francis Maina <sebastianfrancix@gmail.com>
2022-09-29 12:22:26 +0300

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import data from "./data";
+import List from "./List";
+
+function App() {
+  return <h2>Reminder project setup</h2>;
+}
 
 export default App;
