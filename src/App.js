9f381541d8e7c26a9cfed6dc842a480a5a161589
Francis Maina <sebastianfrancix@gmail.com>
2022-09-29 12:30:37 +0300

    added static landing page data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import data from "./data";
 import List from "./List";
 
 function App() {
-  return <h2>Reminder project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>0 Birthdays Today</h3>
+        <List />
+        <button type="button" onClick={() => console.log("I was clicked")}>
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
